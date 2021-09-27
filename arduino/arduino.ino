76606d8f3890843f7640202305a3395837ceb0d1
[fahimali01] <[fahim123rahim]>
2021-09-27 17:53:48 +0500

    baseer

---
+++ arduino/arduino.ino
@@ -0,0 +1,32 @@
+
+int led= 3 ;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  pinMode(led , OUTPUT);
+
+        
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  for (int i=0; i<=255;)
+
+{
+  
+  analogWrite(3, i);      
+  delay(5);
+  
+  }
+   for(int i=255; i>=0; i--)
+   {
+    analogWrite(3, i);
+    delay(5); 
+    
+    }
+  }
